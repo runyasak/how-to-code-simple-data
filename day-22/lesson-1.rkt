d40160b669de3e72c3a38b246e1d490d3efc4683
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-07-24 23:16:36 +0700

    add day-22 with lessons

---
+++ day-22/lesson-1.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname lesson-1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;Encapsulation
+
+(local [(define a 1)
+        (define b 2)]
+  (+ a b))
